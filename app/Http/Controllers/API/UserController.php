e82301d4c63c83398a84ac54537cc69edb7156c2
sukanta555 <38782744+sukanta555@users.noreply.github.com>
2024-08-23 18:00:30 +0530

    JWT API - 1st day
    JWT API registration upload.

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string|min:2|max:150',
+            'email' => 'required|string|email|max:150|unique:users',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'msg' => 'User Inserted Successfully',
+            'user' => $user
+        ], 201);
+    }
+}
